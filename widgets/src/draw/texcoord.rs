bdfd29bac32f1e232544d4a390edf7c74d6be20e
wolfiestyle <slayerbeast@gmail.com>
2020-06-15 16:57:25 -0400

    implemented ops for TexCoord

--- widgets/src/draw/texcoord.rs
+++ widgets/src/draw/texcoord.rs
@@ -1,4 +1,5 @@
 use crate::geometry::Point;
+use std::ops;
 
 /// Texture coordinates (in [0, 1] range).
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
@@ -20,8 +21,56 @@ impl TexCoord {
     }
 
     #[inline]
+    pub fn normalize(self) -> Self {
+        TexCoord {
+            u: self.u % 1.0,
+            v: self.v % 1.0,
+        }
+    }
+
+    #[inline]
     pub fn components(self) -> [f32; 2] {
         [self.u, self.v]
+    }
+
+    #[inline]
+    pub fn map<F>(self, mut f: F) -> Self
+    where
+        F: FnMut(f32) -> f32,
+    {
+        TexCoord {
+            u: f(self.u),
+            v: f(self.v),
+        }
+    }
+
+    #[inline]
+    pub fn map2<F>(self, other: Self, mut f: F) -> Self
+    where
+        F: FnMut(f32, f32) -> f32,
+    {
+        TexCoord {
+            u: f(self.u, other.u),
+            v: f(self.v, other.v),
+        }
+    }
+
+    #[inline]
+    pub fn map_mut<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&mut f32),
+    {
+        f(&mut self.u);
+        f(&mut self.v);
+    }
+
+    #[inline]
+    pub fn map2_mut<F>(&mut self, other: Self, mut f: F)
+    where
+        F: FnMut(&mut f32, f32),
+    {
+        f(&mut self.u, other.u);
+        f(&mut self.v, other.v);
     }
 }
 
@@ -41,10 +90,87 @@ impl From<(f32, f32)> for TexCoord {
 
 impl From<Point<f32>> for TexCoord {
     #[inline]
-    fn from(p: Point<f32>) -> Self {
-        TexCoord {
-            u: p.x % 1.0,
-            v: p.y % 1.0,
-        }
+    fn from(Point { x: u, y: v }: Point<f32>) -> Self {
+        TexCoord { u, v }
+    }
+}
+
+impl ops::Add for TexCoord {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, rhs: Self) -> Self::Output {
+        self.map2(rhs, ops::Add::add)
+    }
+}
+
+impl ops::Sub for TexCoord {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.map2(rhs, ops::Sub::sub)
+    }
+}
+
+impl ops::Mul<f32> for TexCoord {
+    type Output = Self;
+
+    #[inline]
+    fn mul(self, rhs: f32) -> Self::Output {
+        self.map(|a| a * rhs)
+    }
+}
+
+impl ops::Div<f32> for TexCoord {
+    type Output = Self;
+
+    #[inline]
+    fn div(self, rhs: f32) -> Self::Output {
+        self.map(|a| a / rhs)
+    }
+}
+
+impl ops::Rem<f32> for TexCoord {
+    type Output = Self;
+
+    #[inline]
+    fn rem(self, rhs: f32) -> Self::Output {
+        self.map(|a| a % rhs)
+    }
+}
+
+impl ops::AddAssign for TexCoord {
+    #[inline]
+    fn add_assign(&mut self, rhs: Self) {
+        self.map2_mut(rhs, ops::AddAssign::add_assign)
+    }
+}
+
+impl ops::SubAssign for TexCoord {
+    #[inline]
+    fn sub_assign(&mut self, rhs: Self) {
+        self.map2_mut(rhs, ops::SubAssign::sub_assign)
+    }
+}
+
+impl ops::MulAssign<f32> for TexCoord {
+    #[inline]
+    fn mul_assign(&mut self, rhs: f32) {
+        self.map_mut(|a| *a *= rhs)
+    }
+}
+
+impl ops::DivAssign<f32> for TexCoord {
+    #[inline]
+    fn div_assign(&mut self, rhs: f32) {
+        self.map_mut(|a| *a /= rhs)
+    }
+}
+
+impl ops::RemAssign<f32> for TexCoord {
+    #[inline]
+    fn rem_assign(&mut self, rhs: f32) {
+        self.map_mut(|a| *a %= rhs)
     }
 }
