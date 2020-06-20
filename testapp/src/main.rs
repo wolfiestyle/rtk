4c3ab48d812bd7f960822f22cad2c707f63ebdbc
wolfiestyle <slayerbeast@gmail.com>
2020-06-20 15:36:25 -0400

    created macro make_widget_enum
    helper to create enum dispatch types for Widget.

--- testapp/src/main.rs
+++ testapp/src/main.rs
@@ -3,7 +3,7 @@ use widgets::event::{EvState, Event, EventContext, EventResult, MouseButton};
 use widgets::geometry::Rect;
 use widgets::toplevel::TopLevel;
 use widgets::widget::{Widget, WidgetId, Window};
-use widgets::{implement_bounds, implement_objectid, implement_visitable};
+use widgets::{implement_bounds, implement_objectid, implement_visitable, make_widget_enum};
 use widgets_glium::GliumApplication;
 
 #[derive(Debug)]
@@ -65,6 +65,7 @@ implement_objectid!(TestWidget<A>, id);
 implement_bounds!(TestWidget<A>, rect: bounds);
 implement_visitable!(TestWidget<A: Widget>, childs[]);
 
+#[derive(Debug)]
 struct TestWidget2 {
     id: WidgetId,
     bounds: Rect,
@@ -94,33 +95,51 @@ implement_objectid!(TestWidget2, id);
 implement_bounds!(TestWidget2, rect: bounds);
 implement_visitable!(TestWidget2);
 
+make_widget_enum! {
+    #[derive(Debug)]
+    enum Stuff {
+        TestWidget2,
+        Rect,
+    }
+}
+
 fn main() {
     let mut widget = TestWidget {
         bounds: Rect::new([20, 10], [320, 240]),
         color: Color::blue(0.25),
         hover: false,
         id: WidgetId::new(),
-        childs: vec![],
+        childs: Vec::<Stuff>::new(),
     };
 
     let image: ImageRef = Image::from_file("image2.jpg").unwrap().into();
 
-    widget.childs.push(TestWidget2 {
-        id: WidgetId::new(),
-        bounds: Rect::new([10, 20], [100, 100]),
-        image: Image::from_file("image.png").unwrap().into(),
-    });
+    widget.childs.push(
+        TestWidget2 {
+            id: WidgetId::new(),
+            bounds: Rect::new([10, 20], [100, 100]),
+            image: Image::from_file("image.png").unwrap().into(),
+        }
+        .into(),
+    );
 
-    widget.childs.push(TestWidget2 {
-        id: WidgetId::new(),
-        bounds: Rect::new([0, 0], [64, 64]),
-        image: image.clone(),
-    });
-    widget.childs.push(TestWidget2 {
-        id: WidgetId::new(),
-        bounds: Rect::new([0, 0], [45, 30]),
-        image,
-    });
+    widget.childs.push(
+        TestWidget2 {
+            id: WidgetId::new(),
+            bounds: Rect::new([0, 0], [64, 64]),
+            image: image.clone(),
+        }
+        .into(),
+    );
+    widget.childs.push(Rect::new([0, 0], [10, 10]).into());
+    widget.childs.push(
+        TestWidget2 {
+            id: WidgetId::new(),
+            bounds: Rect::new([0, 0], [45, 30]),
+            image,
+        }
+        .into(),
+    );
 
     let mut window = Window::new(widget);
     window.set_title("awoo");
--- widgets/src/widget.rs
+++ widgets/src/widget.rs
@@ -50,3 +50,103 @@ impl<T: Widget> Widget for Box<T> {
         (**self).handle_event(event, ctx)
     }
 }
+
+#[macro_export]
+macro_rules! make_widget_enum {
+    ($(#[$($attr:tt)*])* pub $(($vis:ident))? enum $name:ident { $($type:ident),+ $(,)? }) => {
+        $crate::make_widget_enum!(@do_impl $(#[$($attr)*])*; pub, $($vis)?; $name, $($type),+);
+    };
+
+    ($(#[$($attr:tt)*])* enum $name:ident { $($type:ident),+ $(,)? }) => {
+        $crate::make_widget_enum!(@do_impl $(#[$($attr)*])*;,; $name, $($type),+);
+    };
+
+    (@do_impl $(#[$($attr:tt)*])*; $($pub:ident)?, $($vis:ident)?; $name:ident, $($type:ident),+) => {
+        $(#[$($attr)*])*
+        $($pub)? $(($vis))? enum $name {
+            $($type($type)),+
+        }
+
+        impl $crate::widget::ObjectId for $name {
+            fn get_id(&self) -> $crate::widget::WidgetId {
+                match self {
+                    $($name::$type(a) => a.get_id(),)+
+                }
+            }
+        }
+
+        impl $crate::geometry::Bounds for $name {
+            fn get_position(&self) -> $crate::geometry::Position {
+                match self {
+                    $($name::$type(a) => a.get_position(),)+
+                }
+            }
+
+            fn get_size(&self) -> $crate::geometry::Size {
+                match self {
+                    $($name::$type(a) => a.get_size(),)+
+                }
+            }
+
+            fn set_position(&mut self, position: $crate::geometry::Position) {
+                match self {
+                    $($name::$type(a) => a.set_position(position),)+
+                }
+            }
+
+            fn set_size(&mut self, size: $crate::geometry::Size) {
+                match self {
+                    $($name::$type(a) => a.set_size(size),)+
+                }
+            }
+
+            fn get_bounds(&self) -> $crate::geometry::Rect {
+                match self {
+                    $($name::$type(a) => a.get_bounds(),)+
+                }
+            }
+        }
+
+        impl $crate::visitor::Visitable for $name {
+            fn accept<V: $crate::visitor::Visitor>(&mut self, visitor: &mut V, ctx: V::Context) -> Result<(), V::Return> {
+                match self {
+                    $($name::$type(a) => a.accept(visitor, ctx),)+
+                }
+            }
+
+            fn accept_rev<V: $crate::visitor::Visitor>(&mut self, visitor: &mut V, ctx: V::Context) -> Result<(), V::Return> {
+                match self {
+                    $($name::$type(a) => a.accept_rev(visitor, ctx),)+
+                }
+            }
+        }
+
+        impl $crate::widget::Widget for $name {
+            fn update_layout(&mut self, parent_rect: $crate::geometry::Rect) {
+                match self {
+                    $($name::$type(a) => a.update_layout(parent_rect),)+
+                }
+            }
+
+            fn draw(&self, dc: $crate::draw::DrawContext) {
+                match self {
+                    $($name::$type(a) => a.draw(dc),)+
+                }
+            }
+
+            fn handle_event(&mut self, event: &$crate::event::Event, ctx: $crate::event::EventContext) -> $crate::event::EventResult {
+                match self {
+                    $($name::$type(a) => a.handle_event(event, ctx),)+
+                }
+            }
+        }
+
+        $(
+            impl From<$type> for $name {
+                fn from(val: $type) -> Self {
+                    $name::$type(val)
+                }
+            }
+        )+
+    }
+}
