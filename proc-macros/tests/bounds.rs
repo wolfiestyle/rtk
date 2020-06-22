f94571f5035a4c1ddfbaea3eb839438d3f827018
wolfiestyle <slayerbeast@gmail.com>
2020-06-22 19:17:19 -0400

    make derive Bounds enum test a bit more spicy

--- proc-macros/tests/bounds.rs
+++ proc-macros/tests/bounds.rs
@@ -25,8 +25,8 @@ struct TestTuple2(i32, Size, Position);
 #[derive(Bounds)]
 enum TestEnum {
     First(TestStruct1),
-    Second(TestTuple2),
-    Third(Rect),
+    Second { val: TestTuple2 },
+    Third(Rect, i32),
 }
 
 #[derive(Bounds)]
@@ -79,8 +79,8 @@ fn bounds() {
     assert_eq!(t2.get_bounds(), Rect::new(pos, size));
 
     let e1 = TestEnum::First(s1);
-    let e2 = TestEnum::Second(t2);
-    let e3 = TestEnum::Third(rect);
+    let e2 = TestEnum::Second { val: t2 };
+    let e3 = TestEnum::Third(rect, 42);
     assert_eq!(e1.get_bounds(), rect);
     assert_eq!(e2.get_bounds(), Rect::new(pos, size));
     assert_eq!(e3.get_bounds(), rect);
