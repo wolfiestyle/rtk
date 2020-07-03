70d4cb2e1d8f35623a1a7e33d0a92c689727620e
wolfiestyle <slayerbeast@gmail.com>
2020-07-03 17:23:30 -0400

    this stuff should be on prelude too

--- core/src/prelude.rs
+++ core/src/prelude.rs
@@ -1,6 +1,6 @@
 //! Commonly used imports.
 pub use crate::draw::{Color, DrawContext, TexCoord, Vertex};
 pub use crate::event::{Axis, ButtonState, Event, EventContext, EventResult, Key, KeySide, MouseButton, Pressed, Released};
-pub use crate::geometry::{Alignment, HAlign, Point, Position, Rect, Size, VAlign};
+pub use crate::geometry::{Alignment, Bounds, HAlign, Point, Position, Rect, Size, VAlign};
 pub use crate::toplevel::Window;
-pub use crate::widget::{Widget, WidgetId};
+pub use crate::widget::{ObjectId, Widget, WidgetId};
