525d92dc4e39d28084835f8ffc10a3fb68e7b9a7
wolfiestyle <slayerbeast@gmail.com>
2020-07-11 17:18:51 -0400

    reduced the amount of stuff in prelude

--- core/src/prelude.rs
+++ core/src/prelude.rs
@@ -1,6 +1,6 @@
 //! Commonly used imports.
-pub use crate::draw::{Color, DrawContext, TexCoord, TexRect, Vertex};
+pub use crate::draw::{Color, DrawContext};
 pub use crate::event::{Axis, ButtonState, Event, EventContext, EventResult, Key, KeySide, MouseButton, Pressed, Released};
-pub use crate::geometry::{Alignment, Bounds, HAlign, Point, Position, Rect, Size, VAlign};
+pub use crate::geometry::{Bounds, Position, Rect, Size};
 pub use crate::toplevel::Window;
 pub use crate::widget::{ObjectId, Widget, WidgetId};
