a8c4f3bfde6a454fd9ed32c40fbb4fd6d547d4e3
wolfiestyle <slayerbeast@gmail.com>
2020-07-22 21:38:41 -0400

    moved the backed drawing code into DrawQueue

--- backend-glium/src/queue.rs
+++ backend-glium/src/queue.rs
@@ -1,8 +1,12 @@
+use glium::index::PrimitiveType;
+use glium::texture::{ClientFormat, MipmapsOption, RawImage2d, SrgbTexture2d, TextureCreationError};
+use glium::{uniform, Surface};
 use std::borrow::Cow;
+use std::fmt;
+use weak_table::WeakKeyHashMap;
 use widgets::draw::{Color, DrawBackend, FillMode, TexCoord, TextDrawMode};
-use widgets::geometry::Point;
-use widgets::geometry::Rect;
-use widgets::image::ImageRef;
+use widgets::geometry::{Point, Rect, Size};
+use widgets::image::{Image, ImageData, ImageRef, ImageWeakRef, PixelFormat};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vertex {
@@ -33,49 +37,72 @@ pub enum Primitive {
 
 /// Primitive draw command detail.
 #[derive(Debug, Clone)]
-pub struct DrawCmdPrim {
+struct DrawCmdPrim {
     /// The primitive to draw.
-    pub primitive: Primitive,
+    primitive: Primitive,
     /// Offset inside the shared index buffer on the draw queue.
-    pub idx_offset: usize,
+    idx_offset: usize,
     /// Length of the indices slice.
-    pub idx_len: usize,
+    idx_len: usize,
     /// Image to use for this draw command.
-    pub texture: Option<ImageRef>,
+    texture: Option<ImageRef>,
     /// Clipping viewport.
-    pub viewport: Rect,
+    viewport: Rect,
 }
 
 /// Text draw command detail.
 #[derive(Debug, Clone)]
-pub struct DrawCmdText {
-    pub text: Cow<'static, str>,
-    pub font_desc: Cow<'static, str>,
-    pub mode: TextDrawMode,
-    pub color: Color,
-    pub viewport: Rect,
+struct DrawCmdText {
+    text: Cow<'static, str>,
+    font_desc: Cow<'static, str>,
+    mode: TextDrawMode,
+    color: Color,
+    viewport: Rect,
 }
 
 /// A single draw command.
 #[derive(Debug, Clone)]
-pub enum DrawCommand {
+enum DrawCommand {
     Clear(Color, Rect),
     Primitives(DrawCmdPrim),
     Text(DrawCmdText),
 }
 
 /// Buffer with draw commands to be sent to the backend.
-#[derive(Debug, Clone, Default)]
+//#[derive(Debug)]
 pub struct DrawQueue {
     /// Shared vertex buffer.
-    pub vertices: Vec<Vertex>,
+    vertices: Vec<Vertex>,
     /// Shared index buffer.
-    pub indices: Vec<u32>,
+    indices: Vec<u32>,
     /// List of draw commands to be executed.
-    pub commands: Vec<DrawCommand>,
+    commands: Vec<DrawCommand>,
+    texture_map: WeakKeyHashMap<ImageWeakRef, SrgbTexture2d>,
+    t_white: SrgbTexture2d,
+    program: glium::Program,
+    pub display: glium::Display,
 }
 
 impl DrawQueue {
+    pub fn new(display: glium::Display) -> Self {
+        let vert_src = include_str!("widgets.vert.glsl");
+        let frag_src = include_str!("widgets.frag.glsl");
+        let program = glium::Program::from_source(&display, vert_src, frag_src, None).unwrap();
+
+        let image = RawImage2d::from_raw_rgba(vec![255u8; 4], (1, 1));
+        let t_white = SrgbTexture2d::with_mipmaps(&display, image, MipmapsOption::NoMipmap).unwrap();
+
+        Self {
+            vertices: Default::default(),
+            indices: Default::default(),
+            commands: Default::default(),
+            program,
+            texture_map: Default::default(),
+            t_white,
+            display,
+        }
+    }
+
     /// Clears all data from the draw queue.
     #[inline]
     pub fn clear(&mut self) {
@@ -96,14 +123,12 @@ impl DrawQueue {
 
     /// Adds a clear command to the draw queue.
     #[inline]
-    pub(crate) fn push_clear(&mut self, color: Color, viewport: Rect) {
+    fn push_clear(&mut self, color: Color, viewport: Rect) {
         self.commands.push(DrawCommand::Clear(color, viewport))
     }
 
     /// Adds raw elements to the draw queue.
-    pub(crate) fn push_prim(
-        &mut self, primitive: Primitive, vertices: &[Vertex], indices: &[u32], texture: Option<ImageRef>, viewport: Rect,
-    ) {
+    fn push_prim(&mut self, primitive: Primitive, vertices: &[Vertex], indices: &[u32], texture: Option<ImageRef>, viewport: Rect) {
         // append vertices to the buffer
         let base_vert = self.vertices.len() as u32;
         self.vertices.extend(vertices);
@@ -127,9 +152,7 @@ impl DrawQueue {
 
     /// Adds a draw text command to the draw queue.
     #[inline]
-    pub(crate) fn push_text(
-        &mut self, text: Cow<'static, str>, font_desc: Cow<'static, str>, mode: TextDrawMode, color: Color, viewport: Rect,
-    ) {
+    fn push_text(&mut self, text: Cow<'static, str>, font_desc: Cow<'static, str>, mode: TextDrawMode, color: Color, viewport: Rect) {
         self.commands.push(DrawCommand::Text(DrawCmdText {
             text,
             font_desc,
@@ -137,6 +160,78 @@ impl DrawQueue {
             color,
             viewport,
         }));
+    }
+
+    pub fn load_textures(&mut self) {
+        self.texture_map.remove_expired();
+
+        for cmd in &self.commands {
+            if let DrawCommand::Primitives(DrawCmdPrim { texture: Some(image), .. }) = cmd {
+                let display = &self.display;
+                self.texture_map
+                    .entry(image.clone())
+                    .or_insert_with(|| to_glium_texture(image, display).unwrap());
+            }
+        }
+    }
+
+    /// Runs the stored draw commands.
+    pub fn execute(&self, win_size: Size) {
+        let mut target = self.display.draw();
+        let vertex_buf = glium::VertexBuffer::new(&self.display, &self.vertices).unwrap();
+
+        for drawcmd in &self.commands {
+            match drawcmd {
+                DrawCommand::Clear(color, viewport) => {
+                    if let Some(vp) = viewport.clip_inside(win_size.into()) {
+                        let rect = to_glium_rect(vp, win_size.h);
+                        target.clear(Some(&rect), Some((color.r, color.g, color.b, color.a)), false, None, None);
+                    }
+                }
+                DrawCommand::Primitives(cmd) => {
+                    // clip the viewport against the visible window area
+                    if let Some(scissor) = cmd.viewport.clip_inside(win_size.into()) {
+                        let mode = match cmd.primitive {
+                            Primitive::Points => PrimitiveType::Points,
+                            Primitive::Lines => PrimitiveType::LinesList,
+                            Primitive::Triangles => PrimitiveType::TrianglesList,
+                        };
+                        // indices reference a single shared vertex buffer
+                        let indices = &self.indices[cmd.idx_offset..cmd.idx_offset + cmd.idx_len];
+                        let index_buf = glium::IndexBuffer::new(&self.display, mode, indices).unwrap();
+                        // get texture to use
+                        let texture = cmd
+                            .texture
+                            .as_ref()
+                            .and_then(|img| self.texture_map.get(img))
+                            .unwrap_or(&self.t_white);
+                        // settings for the pipeline
+                        let uniforms = uniform! {
+                            vp_size: <[f32; 2]>::from(win_size.as_point()),
+                            tex: texture.sampled()
+                                .wrap_function(glium::uniforms::SamplerWrapFunction::Repeat)
+                                .minify_filter(glium::uniforms::MinifySamplerFilter::Nearest)
+                                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest)
+                        };
+                        let draw_params = glium::DrawParameters {
+                            blend: glium::Blend::alpha_blending(),
+                            scissor: Some(to_glium_rect(scissor, win_size.h)),
+                            ..Default::default()
+                        };
+                        // perform the draw command
+                        target
+                            .draw(&vertex_buf, &index_buf, &self.program, &uniforms, &draw_params)
+                            .unwrap();
+                    }
+                }
+                DrawCommand::Text(cmd) => {
+                    //TODO: implement text drawing
+                    dbg!(cmd);
+                }
+            }
+        }
+
+        target.finish().unwrap();
     }
 }
 
@@ -173,5 +268,90 @@ impl DrawBackend for DrawQueue {
     #[inline]
     fn draw_text(&mut self, text: &str, font_desc: &str, mode: TextDrawMode, color: Color, viewport: Rect) {
         self.push_text(text.to_owned().into(), font_desc.to_owned().into(), mode, color, viewport)
+    }
+}
+
+impl fmt::Debug for DrawQueue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("GliumWindow")
+            .field("vertices", &self.vertices)
+            .field("indices", &self.indices)
+            .field("commands", &self.commands)
+            .field("texture_map", &self.texture_map)
+            .field("t_white", &self.t_white)
+            .field("program", &self.program)
+            .finish()
+    }
+}
+
+fn to_glium_rect(rect: widgets::geometry::Rect, win_height: u32) -> glium::Rect {
+    glium::Rect {
+        left: rect.pos.x as u32,
+        bottom: win_height - rect.size.h - rect.pos.y as u32,
+        width: rect.size.w,
+        height: rect.size.h,
+    }
+}
+
+fn to_glium_texture(image: &Image, display: &glium::Display) -> Result<SrgbTexture2d, TextureCreationError> {
+    let (width, height) = image.get_size().into();
+    match image.get_data() {
+        None => SrgbTexture2d::empty(display, width, height),
+        Some(ImageData::U8(vec)) => {
+            let img = RawImage2d {
+                data: Cow::Borrowed(vec),
+                width,
+                height,
+                format: match image.get_format() {
+                    PixelFormat::Luma => ClientFormat::U8,
+                    PixelFormat::LumaA => ClientFormat::U8U8,
+                    PixelFormat::Rgb => ClientFormat::U8U8U8,
+                    PixelFormat::Rgba => ClientFormat::U8U8U8U8,
+                },
+            };
+            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
+        }
+        Some(ImageData::U16(vec)) => {
+            let img = RawImage2d {
+                data: Cow::Borrowed(vec),
+                width,
+                height,
+                format: match image.get_format() {
+                    PixelFormat::Luma => ClientFormat::U16,
+                    PixelFormat::LumaA => ClientFormat::U16U16,
+                    PixelFormat::Rgb => ClientFormat::U16U16U16,
+                    PixelFormat::Rgba => ClientFormat::U16U16U16U16,
+                },
+            };
+            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
+        }
+        Some(ImageData::U32(vec)) => {
+            let img = RawImage2d {
+                data: Cow::Borrowed(vec),
+                width,
+                height,
+                format: match image.get_format() {
+                    PixelFormat::Luma => ClientFormat::U32,
+                    PixelFormat::LumaA => ClientFormat::U32U32,
+                    PixelFormat::Rgb => ClientFormat::U32U32U32,
+                    PixelFormat::Rgba => ClientFormat::U32U32U32U32,
+                },
+            };
+            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
+        }
+        Some(ImageData::F32(vec)) => {
+            let img = RawImage2d {
+                data: Cow::Borrowed(vec),
+                width,
+                height,
+                format: match image.get_format() {
+                    PixelFormat::Luma => ClientFormat::F32,
+                    PixelFormat::LumaA => ClientFormat::F32F32,
+                    PixelFormat::Rgb => ClientFormat::F32F32F32,
+                    PixelFormat::Rgba => ClientFormat::F32F32F32F32,
+                },
+            };
+            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
+        }
     }
 }
--- backend-glium/src/window.rs
+++ backend-glium/src/window.rs
@@ -1,27 +1,17 @@
-use crate::queue::{DrawCmdPrim, DrawCommand, DrawQueue, Primitive};
+use crate::queue::DrawQueue;
 use glium::glutin::dpi::PhysicalPosition;
 use glium::glutin::event_loop::EventLoop;
 use glium::glutin::window::WindowId;
 use glium::glutin::{ContextBuilder, GlProfile, Robustness};
-use glium::index::PrimitiveType;
-use glium::texture::{ClientFormat, MipmapsOption, RawImage2d, SrgbTexture2d, TextureCreationError};
-use glium::{uniform, Surface};
-use std::borrow::Cow;
-use std::fmt;
-use weak_table::WeakKeyHashMap;
 use widgets::event::Event;
-use widgets::image::{Image, ImageData, ImageWeakRef, PixelFormat};
 use widgets::toplevel::{TopLevel, WindowAttributes};
 use widgets_winit::{make_win_builder, BackendWindow};
 
+#[derive(Debug)]
 pub struct GliumWindow<T> {
-    display: glium::Display,
-    program: glium::Program,
-    t_white: SrgbTexture2d,
-    texture_map: WeakKeyHashMap<ImageWeakRef, SrgbTexture2d>,
     draw_queue: DrawQueue,
     cur_attr: WindowAttributes,
-    pub window: T,
+    window: T,
 }
 
 impl<T: TopLevel> GliumWindow<T> {
@@ -43,97 +33,23 @@ impl<T: TopLevel> GliumWindow<T> {
             display.gl_window().window().set_outer_position(PhysicalPosition::new(pos.x, pos.y));
         }
 
-        let vert_src = include_str!("widgets.vert.glsl");
-        let frag_src = include_str!("widgets.frag.glsl");
-        let program = glium::Program::from_source(&display, vert_src, frag_src, None).unwrap();
-
-        let image = RawImage2d::from_raw_rgba(vec![255u8; 4], (1, 1));
-        let t_white = SrgbTexture2d::with_mipmaps(&display, image, MipmapsOption::NoMipmap).unwrap();
+        let draw_queue = DrawQueue::new(display);
 
         Self {
-            display,
-            program,
-            t_white,
-            texture_map: Default::default(),
-            draw_queue: Default::default(),
+            draw_queue,
             cur_attr: win_attr.clone(),
             window,
         }
     }
 
-    fn draw_elements(&self, target: &mut glium::Frame) {
-        let win_size = self.window.get_attr().size;
-        let vertex_buf = glium::VertexBuffer::new(&self.display, &self.draw_queue.vertices).unwrap();
-
-        for drawcmd in &self.draw_queue.commands {
-            match drawcmd {
-                DrawCommand::Clear(color, viewport) => {
-                    if let Some(vp) = viewport.clip_inside(win_size.into()) {
-                        let rect = to_glium_rect(vp, win_size.h);
-                        target.clear(Some(&rect), Some((color.r, color.g, color.b, color.a)), false, None, None);
-                    }
-                }
-                DrawCommand::Primitives(cmd) => {
-                    // clip the viewport against the visible window area
-                    if let Some(scissor) = cmd.viewport.clip_inside(win_size.into()) {
-                        let mode = match cmd.primitive {
-                            Primitive::Points => PrimitiveType::Points,
-                            Primitive::Lines => PrimitiveType::LinesList,
-                            Primitive::Triangles => PrimitiveType::TrianglesList,
-                        };
-                        // indices reference a single shared vertex buffer
-                        let indices = &self.draw_queue.indices[cmd.idx_offset..cmd.idx_offset + cmd.idx_len];
-                        let index_buf = glium::IndexBuffer::new(&self.display, mode, indices).unwrap();
-                        // get texture to use
-                        let texture = cmd
-                            .texture
-                            .as_ref()
-                            .and_then(|img| self.texture_map.get(img))
-                            .unwrap_or(&self.t_white);
-                        // settings for the pipeline
-                        let uniforms = uniform! {
-                            vp_size: <[f32; 2]>::from(win_size.as_point()),
-                            tex: texture.sampled()
-                                .wrap_function(glium::uniforms::SamplerWrapFunction::Repeat)
-                                .minify_filter(glium::uniforms::MinifySamplerFilter::Nearest)
-                                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest)
-                        };
-                        let draw_params = glium::DrawParameters {
-                            blend: glium::Blend::alpha_blending(),
-                            scissor: Some(to_glium_rect(scissor, win_size.h)),
-                            ..Default::default()
-                        };
-                        // perform the draw command
-                        target
-                            .draw(&vertex_buf, &index_buf, &self.program, &uniforms, &draw_params)
-                            .unwrap();
-                    }
-                }
-                DrawCommand::Text(cmd) => {
-                    //TODO: implement text drawing
-                    dbg!(cmd);
-                }
-            }
-        }
-    }
-
-    fn load_textures(&mut self) {
-        self.texture_map.remove_expired();
-
-        for cmd in &self.draw_queue.commands {
-            if let DrawCommand::Primitives(DrawCmdPrim { texture: Some(image), .. }) = cmd {
-                let display = &self.display;
-                self.texture_map
-                    .entry(image.clone())
-                    .or_insert_with(|| to_glium_texture(image, display).unwrap());
-            }
-        }
+    fn display(&self) -> &glium::Display {
+        &self.draw_queue.display
     }
 }
 
 impl<T: TopLevel> BackendWindow for GliumWindow<T> {
     fn get_id(&self) -> WindowId {
-        self.display.gl_window().window().id()
+        self.display().gl_window().window().id()
     }
 
     fn update(&mut self) {
@@ -144,14 +60,12 @@ impl<T: TopLevel> BackendWindow for GliumWindow<T> {
     fn draw(&mut self) {
         self.draw_queue.clear();
         self.window.draw(&mut self.draw_queue);
-        self.load_textures();
-        let mut target = self.display.draw();
-        self.draw_elements(&mut target);
-        target.finish().unwrap();
+        self.draw_queue.load_textures();
+        self.draw_queue.execute(self.cur_attr.size);
     }
 
     fn request_redraw(&self) {
-        self.display.gl_window().window().request_redraw();
+        self.display().gl_window().window().request_redraw();
     }
 
     fn push_event(&mut self, event: Event) -> bool {
@@ -167,90 +81,5 @@ impl<T: TopLevel> BackendWindow for GliumWindow<T> {
             _ => (),
         }
         self.window.push_event(event)
-    }
-}
-
-impl<T: fmt::Debug> fmt::Debug for GliumWindow<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("GliumWindow")
-            .field("program", &self.program)
-            .field("t_white", &self.t_white)
-            .field("texture_map", &self.texture_map)
-            .field("draw_queue", &self.draw_queue)
-            .field("cur_attr", &self.cur_attr)
-            .field("window", &self.window)
-            .finish()
-    }
-}
-
-fn to_glium_rect(rect: widgets::geometry::Rect, win_height: u32) -> glium::Rect {
-    glium::Rect {
-        left: rect.pos.x as u32,
-        bottom: win_height - rect.size.h - rect.pos.y as u32,
-        width: rect.size.w,
-        height: rect.size.h,
-    }
-}
-
-fn to_glium_texture(image: &Image, display: &glium::Display) -> Result<SrgbTexture2d, TextureCreationError> {
-    let (width, height) = image.get_size().into();
-    match image.get_data() {
-        None => SrgbTexture2d::empty(display, width, height),
-        Some(ImageData::U8(vec)) => {
-            let img = RawImage2d {
-                data: Cow::Borrowed(vec),
-                width,
-                height,
-                format: match image.get_format() {
-                    PixelFormat::Luma => ClientFormat::U8,
-                    PixelFormat::LumaA => ClientFormat::U8U8,
-                    PixelFormat::Rgb => ClientFormat::U8U8U8,
-                    PixelFormat::Rgba => ClientFormat::U8U8U8U8,
-                },
-            };
-            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
-        }
-        Some(ImageData::U16(vec)) => {
-            let img = RawImage2d {
-                data: Cow::Borrowed(vec),
-                width,
-                height,
-                format: match image.get_format() {
-                    PixelFormat::Luma => ClientFormat::U16,
-                    PixelFormat::LumaA => ClientFormat::U16U16,
-                    PixelFormat::Rgb => ClientFormat::U16U16U16,
-                    PixelFormat::Rgba => ClientFormat::U16U16U16U16,
-                },
-            };
-            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
-        }
-        Some(ImageData::U32(vec)) => {
-            let img = RawImage2d {
-                data: Cow::Borrowed(vec),
-                width,
-                height,
-                format: match image.get_format() {
-                    PixelFormat::Luma => ClientFormat::U32,
-                    PixelFormat::LumaA => ClientFormat::U32U32,
-                    PixelFormat::Rgb => ClientFormat::U32U32U32,
-                    PixelFormat::Rgba => ClientFormat::U32U32U32U32,
-                },
-            };
-            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
-        }
-        Some(ImageData::F32(vec)) => {
-            let img = RawImage2d {
-                data: Cow::Borrowed(vec),
-                width,
-                height,
-                format: match image.get_format() {
-                    PixelFormat::Luma => ClientFormat::F32,
-                    PixelFormat::LumaA => ClientFormat::F32F32,
-                    PixelFormat::Rgb => ClientFormat::F32F32F32,
-                    PixelFormat::Rgba => ClientFormat::F32F32F32F32,
-                },
-            };
-            SrgbTexture2d::with_mipmaps(display, img, MipmapsOption::NoMipmap)
-        }
     }
 }
