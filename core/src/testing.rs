9e5b320ec24864263e00024c2ba60b923151bcd9
wolfiestyle <slayerbeast@gmail.com>
2020-08-14 16:51:28 -0400

    made a more elaborate Widget derive test

--- core/src/testing.rs
+++ core/src/testing.rs
@@ -1,38 +1,102 @@
-use crate::backend::{Resources, TextureError};
-use crate::draw::TextureId;
+use crate::backend::{DrawBackend, Resources, TextureError};
+use crate::draw::{Color, ColorOp, TexCoord, TextSection, TextureId};
 use crate::font::{FontFamily, FontId, FontLoadError, FontProperties, FontSource};
+use crate::geometry::{Point, Rect};
 use crate::image::Image;
+use std::collections::HashMap;
 
-/// Null resources implementation. Useful for testing.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
-pub struct NullResources;
+/// Test backend implementation.
+///
+/// It does nothing but storing the values it receives.
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct TestBackend {
+    pub textures: HashMap<TextureId, Image>,
+    pub fonts: Vec<FontSource>,
+    pub draw_cmd: Vec<TestDrawCmd>,
+}
 
-impl Resources for NullResources {
-    #[inline]
-    fn load_texture(&mut self, _id: TextureId, _image: &Image) -> Result<(), TextureError> {
-        Err(TextureError::FormatNotSupported)
+impl Resources for TestBackend {
+    fn load_texture(&mut self, id: TextureId, image: &Image) -> Result<(), TextureError> {
+        self.textures.insert(id, image.clone());
+        Ok(())
     }
 
-    #[inline]
-    fn load_texture_once(&mut self, _id: TextureId, _image: &Image) -> Result<(), TextureError> {
-        Err(TextureError::FormatNotSupported)
+    fn load_texture_once(&mut self, id: TextureId, image: &Image) -> Result<(), TextureError> {
+        self.textures.entry(id).or_insert_with(|| image.clone());
+        Ok(())
     }
 
-    #[inline]
-    fn delete_texture(&mut self, _id: TextureId) {}
+    fn delete_texture(&mut self, id: TextureId) {
+        self.textures.remove(&id);
+    }
 
-    #[inline]
     fn enumerate_fonts(&self) -> Vec<String> {
-        vec![]
+        unimplemented!()
     }
 
-    #[inline]
     fn select_font(&self, _family_names: &[FontFamily], _properties: &FontProperties) -> Option<FontSource> {
-        None
+        unimplemented!()
     }
 
-    #[inline]
-    fn load_font(&mut self, _font_src: &FontSource) -> Result<FontId, FontLoadError> {
-        Err(FontLoadError::InvalidData)
+    fn load_font(&mut self, font_src: &FontSource) -> Result<FontId, FontLoadError> {
+        let id = self.fonts.len();
+        self.fonts.push(font_src.clone());
+        Ok(FontId(id))
     }
+}
+
+impl DrawBackend for TestBackend {
+    type Vertex = TestVertex;
+
+    fn clear(&mut self, color: Color, viewport: Rect) {
+        self.draw_cmd.push(TestDrawCmd::Clear { color, viewport });
+    }
+
+    fn draw_triangles<V, I>(&mut self, vertices: V, indices: I, texture: Option<TextureId>, viewport: Rect)
+    where
+        V: IntoIterator<Item = Self::Vertex>,
+        I: IntoIterator<Item = u32>,
+    {
+        self.draw_cmd.push(TestDrawCmd::Triangles {
+            vertices: vertices.into_iter().collect(),
+            indices: indices.into_iter().collect(),
+            texture,
+            viewport,
+        })
+    }
+
+    fn draw_text(&mut self, text: TextSection, viewport: Rect) {
+        self.draw_cmd.push(TestDrawCmd::Text { text, viewport })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct TestVertex {
+    pub pos: Point<f32>,
+    pub color: ColorOp,
+    pub texc: TexCoord,
+}
+
+impl From<(Point<f32>, ColorOp, TexCoord)> for TestVertex {
+    fn from((pos, color, texc): (Point<f32>, ColorOp, TexCoord)) -> Self {
+        TestVertex { pos, color, texc }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TestDrawCmd {
+    Clear {
+        color: Color,
+        viewport: Rect,
+    },
+    Triangles {
+        vertices: Vec<TestVertex>,
+        indices: Vec<u32>,
+        texture: Option<TextureId>,
+        viewport: Rect,
+    },
+    Text {
+        text: TextSection,
+        viewport: Rect,
+    },
 }
--- proc-macros/tests/widget.rs
+++ proc-macros/tests/widget.rs
@@ -1,5 +1,7 @@
+use std::sync::mpsc::{channel, Sender};
+use widgets::draw::{DrawContext, Text, TextSection};
 use widgets::prelude::*;
-use widgets::testing::NullResources;
+use widgets::testing::{TestBackend, TestDrawCmd};
 use widgets::widget::Empty;
 use widgets_derive::{Bounds, ObjectId, Visitable, Widget};
 
@@ -14,30 +16,70 @@ enum TestEnum<T> {
 struct TestWidget {
     id: WidgetId,
     bounds: Rect,
+    events: Sender<Event>,
 }
 
 impl Widget for TestWidget {
     fn update_layout<R: Resources>(&mut self, parent_rect: Rect, _resources: &mut R) {
         self.bounds = parent_rect;
     }
-    fn draw<B: DrawBackend>(&self, _dc: DrawContext<B>) {}
-    fn handle_event(&mut self, _event: &Event, _ctx: EventContext) -> EventResult {
-        EventResult::Pass
+    fn draw<B: DrawBackend>(&self, mut dc: DrawContext<B>) {
+        dc.draw_text(TextSection::default().add_text(Text::new("asdf")));
     }
-    fn event_consumed(&mut self, _event: &Event, _ctx: &EventContext) {}
+
+    fn handle_event(&mut self, event: &Event, _ctx: EventContext) -> EventResult {
+        self.events.send(event.clone()).unwrap();
+        EventResult::Consumed
+    }
+
+    fn event_consumed(&mut self, event: &Event, _ctx: &EventContext) {
+        self.events.send(event.clone()).unwrap();
+    }
+
+    fn viewport_origin(&self) -> Position {
+        [11, 22].into()
+    }
 }
 
 #[test]
 fn widget() {
     let rect1 = Rect::new([0, 1], [13, 42]);
     let rect2 = Rect::new([10, 20], [320, 240]);
-    let mut e1: TestEnum<Empty> = TestEnum::Empty(rect1.into());
+    let (events, ev_recv) = channel();
+
+    let mut e1: TestEnum<TestWidget> = TestEnum::Empty(rect1.into());
     let mut e2 = TestEnum::Other(TestWidget {
         id: WidgetId::new(),
         bounds: rect1,
+        events,
     });
-    e1.update_layout(rect2, &mut NullResources);
-    e2.update_layout(rect2, &mut NullResources);
+
+    let mut backend = TestBackend::default();
+
+    e1.update_layout(rect2, &mut backend);
+    e2.update_layout(rect2, &mut backend);
     assert_eq!(e1.get_bounds(), rect1);
     assert_eq!(e2.get_bounds(), rect2);
+
+    e2.draw(DrawContext::new(&mut backend, Rect::new_at_origin([800, 600])));
+    if let Some(TestDrawCmd::Text {
+        text: TextSection { text, .. },
+        ..
+    }) = &backend.draw_cmd.get(0)
+    {
+        assert_eq!(text[0].text, "asdf");
+    } else {
+        panic!("backend.draw_cmd empty")
+    }
+
+    let ctx = unsafe { std::mem::zeroed() }; // value is never read, so it should be safe
+    let res = e2.handle_event(&Event::Created, ctx);
+    assert_eq!(res, EventResult::Consumed);
+    assert_eq!(ev_recv.try_recv(), Ok(Event::Created));
+    e2.event_consumed(&Event::CloseRequest, &ctx);
+    assert_eq!(ev_recv.try_recv(), Ok(Event::CloseRequest));
+    std::mem::forget(ctx); // make sure it's not calling Drop
+
+    let vp_orig = e2.viewport_origin();
+    assert_eq!(vp_orig, Position::new(11, 22));
 }
