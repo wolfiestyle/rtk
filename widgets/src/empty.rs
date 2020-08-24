0770680d6ec32ef59524ee3d6ca15452347351d9
wolfiestyle <slayerbeast@gmail.com>
2020-08-24 18:30:46 -0400

    use derives to implement parts of Empty

--- widgets/src/empty.rs
+++ widgets/src/empty.rs
@@ -1,15 +1,11 @@
-use rtk::backend::{DrawBackend, Resources};
-use rtk::draw::DrawContext;
-use rtk::event::{Event, EventContext, EventResult};
-use rtk::geometry::{Bounds, BoundsMut, Position, Rect, Size};
-use rtk::visitor::{Visitable, Visitor};
-use rtk::widget::{ObjectId, Widget, WidgetId};
+use rtk::prelude::*;
+use rtk_derive::{Bounds, Visitable};
 
 /// The empty widget.
 ///
 /// It's a "null" widget that does nothing (it only occupies space).
 /// Can be used as a filler in layout definitions.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default, Bounds, Visitable)]
 pub struct Empty {
     bounds: Rect,
 }
@@ -37,54 +33,6 @@ impl ObjectId for Empty {
     #[inline]
     fn get_id(&self) -> WidgetId {
         WidgetId::NONE
-    }
-}
-
-impl Bounds for Empty {
-    #[inline]
-    fn get_position(&self) -> Position {
-        self.bounds.pos
-    }
-
-    #[inline]
-    fn get_size(&self) -> Size {
-        self.bounds.size
-    }
-
-    #[inline]
-    fn get_bounds(&self) -> Rect {
-        self.bounds
-    }
-}
-
-impl BoundsMut for Empty {
-    #[inline]
-    fn set_position(&mut self, position: Position) {
-        self.bounds.pos = position;
-    }
-
-    #[inline]
-    fn set_size(&mut self, size: Size) {
-        self.bounds.size = size;
-    }
-
-    #[inline]
-    fn set_bounds(&mut self, bounds: Rect) {
-        self.bounds = bounds;
-    }
-}
-
-impl Visitable for Empty {
-    #[inline]
-    fn accept<V: Visitor>(&mut self, visitor: V, prev_ctx: &V::Context) -> V {
-        if visitor.finished() {
-            return visitor;
-        }
-        if let Some(ctx) = visitor.new_context(self, prev_ctx) {
-            visitor.visit_before(self, &ctx).visit_after(self, &ctx)
-        } else {
-            visitor
-        }
     }
 }
 
